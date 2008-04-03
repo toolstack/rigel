35a90fda80226e3806e4cc5ed93dfd8da845ecd0
gregross <gregross@928a71aa-a348-0410-8869-e2fae452ace6>
2008-04-03 22:41:41 +0000

    - post beta 2 version update

--- Trunk/RIGELLIB/Config.pm
+++ Trunk/RIGELLIB/Config.pm
@@ -36,7 +36,7 @@ package RIGELLIB::Config;
 
     our $this = undef;
 
-    our $VERSION = "V1b2";
+    our $VERSION = "V1 post b2 development";
 
     # fallback config value(real default value)
     # this value may be overridden by config file and cmdline option.
