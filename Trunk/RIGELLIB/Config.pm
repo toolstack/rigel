15ecda7c30c3bae643b8e4e086cb18ea44972ac1
Greg Ross <greg@toolstack.com>
2008-06-11 01:19:51 +0000

    - Post b3 version update

--- Trunk/RIGELLIB/Config.pm
+++ Trunk/RIGELLIB/Config.pm
@@ -36,7 +36,7 @@ package RIGELLIB::Config;
 
     our $this = undef;
 
-    our $VERSION = "V1b3";
+    our $VERSION = "V1 Post b3 Development";
 
     # fallback config value(real default value)
     # this value may be overridden by config file and cmdline option.
