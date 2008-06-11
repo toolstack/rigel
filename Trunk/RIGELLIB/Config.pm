5bd57dd4d281220429a56818a63fb68aa107dfa7
Greg Ross <greg@toolstack.com>
2008-06-11 01:02:25 +0000

    - Beta 3 release updates

--- Trunk/RIGELLIB/Config.pm
+++ Trunk/RIGELLIB/Config.pm
@@ -36,7 +36,7 @@ package RIGELLIB::Config;
 
     our $this = undef;
 
-    our $VERSION = "V1 post b2 development";
+    our $VERSION = "V1b3";
 
     # fallback config value(real default value)
     # this value may be overridden by config file and cmdline option.
