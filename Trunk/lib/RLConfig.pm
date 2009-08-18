3dfcb2897e753bbccd3535d23b77e1db4f5b7371
Greg Ross <greg@toolstack.com>
2009-08-18 19:11:42 +0000

    - Update for RC1 release

--- Trunk/lib/RLConfig.pm
+++ Trunk/lib/RLConfig.pm
@@ -42,7 +42,7 @@ package RLConfig;
     @ISA = qw(Exporter);
     @EXPORT_OK = qw(LoadConfig ParseConfigString GetVersion GetGlobalConfig GetSiteConfig ApplyTemplate);
 
-    our $VERSION = "V1 post-b5 development";
+    our $VERSION = "V1RC1";
 
     # fallback config value(real default value)
     # these values may be overridden by the config file or cmdline options.
