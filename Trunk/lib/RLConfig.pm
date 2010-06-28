312fa1c9860b6ae93494e8b5a37dac22d2c34b16
gregross <gregross@928a71aa-a348-0410-8869-e2fae452ace6>
2010-06-28 18:53:57 +0000

    - update for V2RC1 release

--- Trunk/lib/RLConfig.pm
+++ Trunk/lib/RLConfig.pm
@@ -42,7 +42,7 @@ package RLConfig;
     @ISA = qw(Exporter);
     @EXPORT_OK = qw(LoadConfig ParseConfigString GetVersion GetGlobalConfig GetSiteConfig ApplyTemplate);
 
-    our $VERSION = "V1.0 post release development";
+    our $VERSION = "V2.0RC1";
 
     # fallback config value (real default value)
     # these values may be overridden by the config file or cmdline options.
